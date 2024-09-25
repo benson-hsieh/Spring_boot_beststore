a984ed54b7458851b663913cd2d175180f7f5c70
DESKTOP-G4QB3JO\謝忠諺 <benson8912@gmail.com>
2024-09-25 21:28:42 +0800

    update view(category column)

--- src/main/java/com/bootsmytool/beststore/controllers/ProductsController.java
+++ src/main/java/com/bootsmytool/beststore/controllers/ProductsController.java
@@ -55,7 +55,7 @@ public class ProductsController {
 			return "products/CreateProduct";
 		}
 		
-		//save image file
+		//************************** save image file *****************************
 		MultipartFile image = productDto.getImageFile();
 		Date createdAt = new Date();
 		String storageFileName = createdAt.getTime() + "_" + image.getOriginalFilename();
